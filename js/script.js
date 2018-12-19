fa540a8e8f83226757a75bdf6016f2074e79f368
rdelva <rdelva@users.noreply.github.com>
2018-12-18 21:07:12 -0500

    figuring out loop

--- js/script.js
+++ js/script.js
@@ -15,15 +15,19 @@ function captionListener () {
 	const video = document.querySelector('video');
 	let trackTime;
 
-	console.log(span[0].dataset);
+	console.log(span);
 	video.addEventListener('timeupdate', function() {
 		//console.log (span[0].dataset.start);
 		
 		let timeStamp   = video.currentTime;
 
-		for (let i = 0; i <= span.length; i++ ) {
+		for (let i = 1; i <= span.length; i++ ) {
 
-			console.log(span[i].dataset);
+			if (timeStamp >= span[i].dataset.start && timeStamp <= span[i].dataset.end ) {
+				
+				span[i].className('highlight');
+			}
+
 		
 		}
 
