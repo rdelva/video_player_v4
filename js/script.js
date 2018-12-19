2b84e03652519ed216aa153411e8fbb741a0224d
rdelva <rdelva@users.noreply.github.com>
2018-12-18 21:04:31 -0500

    addeing in  media player console

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 
 $('video').mediaelementplayer({
-	features: ['playpause', 'progress', 'tracks', 'volume', 'fullscreen']
+	features: ['playpause', 'progress', 'tracks', 'volume', 'fullscreen'],
+	startLangauge: 'en'
 });
+
+
+function captionListener () {
+
+	const p = document.getElementsByTagName('p');
+	const caption = document.querySelector('.caption');
+	const span = document.querySelectorAll('p span');
+	const t_start = document.querySelectorAll('[data-start]');
+	const t_end = document.querySelectorAll('[data-end]');
+	const video = document.querySelector('video');
+	let trackTime;
+
+	console.log(span[0].dataset);
+	video.addEventListener('timeupdate', function() {
+		//console.log (span[0].dataset.start);
+		
+		let timeStamp   = video.currentTime;
+
+		for (let i = 0; i <= span.length; i++ ) {
+
+			console.log(span[i].dataset);
+		
+		}
+
+	}); 
+
+
+
+
+
+}
+
+captionListener();
